a5a8050449270042ade73c4c3ef41bfad4626e45
Dane Miller <unfaiyted@gmail.com>
2018-07-26 08:18:02 -0500

    first index

---
+++ app/index.js
@@ -0,0 +1,19 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+require('./index.css');
+
+
+// state
+// lifestyle event
+// UI
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello World!
+            </div>
+        )
+    }
+}
