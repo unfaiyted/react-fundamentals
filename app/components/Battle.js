11ffe6ec0a5be269274160d3c784c58ee60cd36b
Dane Miller <unfaiyted@gmail.com>
2018-07-27 16:58:54 -0500

    working on the api to configure with github

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,12 +1,176 @@
 var React = require('react');
+var PropTypes = require('prop-types');
+var Link = require('react-router-dom').Link;
 
 
+function PlayerPreview(props) {
+    return (
+        <div>
+            <div className='column'>
+                <img
+                    className='avatar'
+                    src={props.avatar}
+                    alt={'Avatar for ' + props.username}
+                    />
+                <h2 className='username'>@{props.username}</h2>
+            </div>
+            <button
+            className='reset'
+            onClick={props.onReset.bind(null, props.id)}>
+                Reset
+            </button>
+        </div>
+    )
+}
 
-class Battle extends React.Component {
+PlayerPreview.propTypes = {
+    avatar: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    onReset: PropTypes.func.isRequired,
+}
+
+class PlayerInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+    }
+
+    handleChange(event) {
+        var value  = event.target.value;
+        this.setState(function() {
+            return {
+                username: value
+            }
+        });
+    }
+
+
+    handleSubmit(event) {
+        event.preventDefault();
+        this.props.onSubmit(
+            this.props.id,
+            this.state.username
+        )
+    }
+
     render() {
         return (
+            <form className='column' onSubmit={this.handleSubmit}>
+                <label className='header' htmlFor='username'>
+                    {this.props.label}
+                </label>
+                <input id='username'
+                       placeholder='github username'
+                        type='text'
+                        autoComplete='off'
+                    value={this.state.username}
+                onChange={this.handleChange}/>
+                <button
+                    className='button'
+                    type='submit'
+                    disabled={!this.state.username}
+                >Submit</button>
+            </form>
+        )
+    }
+}
+
+
+PlayerInput.propTypes = {
+    id: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    onSubmit: PropTypes.func.isRequired,
+
+};
+
+PlayerInput.defaultProps = {
+    label: 'Username'
+}
+
+class Battle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            playerOneName: '',
+            playerTwoName: '',
+            playerOneImg: null,
+            playerTwoImg: null,
+        }
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleReset = this.handleReset.bind(this);
+    }
+
+    handleReset(id) {
+        this.setState(function() {
+            var newState = {};
+            newState[id + 'Name'] = '';
+            newState[id + 'Img'] = null;
+            return newState;
+        })
+    }
+
+    handleSubmit(id, username) {
+        this.setState(function() {
+            var newState = {};
+            newState[id + 'Name'] = username;
+            newState[id + 'Img'] = 'https://github.com/' + username + '.png?size=200';
+            return newState;
+        })
+    }
+
+
+    render() {
+        var match = this.props.match;
+        var playerOneName = this.state.playerOneName;
+        var playerTwoName = this.state.playerTwoName;
+        var playerOneImg = this.state.playerOneImg;
+        var playerTwoImg = this.state.playerTwoImg;
+
+        return (
                 <div>
-                    BATTLEEE
+                    <div className='row'>
+
+                        {!playerOneName &&
+                        <PlayerInput
+                        id='playerOne'
+                        label='Player One'
+                        onSubmit={this.handleSubmit}/>}
+
+                        {playerOneImg !== null &&
+                        <PlayerPreview avatar={playerOneImg} username={playerOneName}
+                                       id='playerOne' onReset={this.handleReset}/>}
+
+                        {!playerTwoName &&
+                        <PlayerInput id='playerTwo'
+                        label='Player Two'
+                        onSubmit={this.handleSubmit}/>}
+
+                        {playerTwoImg !== null &&
+                        <PlayerPreview avatar={playerTwoImg} username={playerTwoName}
+                                       id='playerTwo' onReset={this.handleReset}/>}
+
+                        {playerOneImg && playerTwoImg &&
+                        <Link
+                            className='button'
+                            to={{
+                                pathname: match.url + '/results',
+                                search: '?playerOneName=' + playerOneName +
+                                    '&playerTwoName=' + playerTwoName
+                            }
+                            }>
+                            Battle!
+                        </Link>
+                        }
+
+                    </div>
                 </div>
         )
     }
--- app/utils/api.js
+++ app/utils/api.js
@@ -1,7 +1,76 @@
 var axios = require('axios');
 
+// deal with rate limiting with api key
+// add if rate limiting issues..
+var id = "737aa6f73426cec2945a";
+var sec = "59b2b18ab7b6716d66a7f4a898f780fd5b808ab5";
+var params = "?client_id=" + id + "&client_secret=" + sec;
+
+
+function getProfile(username) {
+    return axios.get('https://api.gethub.com/users/' + username + params)
+        .then(function (user) {
+            //formats data before passing it back to the function
+           return user.data;
+        });
+}
+
+function getRepos(username) {
+    return axios.get('https://api.gethub.com/users/' + username + '/repos ' + params +
+    '&per_page=100')
+
+}
+
+function getStarCount(repos) {
+    return repos.data.reduce(function(count, repo) {
+        return count + repo.stargazers_count;
+    }, 0)
+}
+
+function calculateScore(profile, repos) {
+    var followers = profile.followers;
+    var totalStars = getStarCount(repos);
+
+    return (followers * 3) + totalStars;
+}
+
+
+function handleError(error) {
+    console.warn(error);
+    return null;
+}
+
+
+// When both have returned with data
+function getUserData(player) {
+    return axios.all([
+        getProfile(player),
+        getRepos(player)
+    ]).then(function(data) {
+        var profile = data[0];
+        var repos = data[1];
+
+        return {
+            profile: profile,
+            score: calculateScore(profile, repos)
+        }
+
+    })
+}
+
+function sortPlayers(players) {
+    return players.sort(function(a,b) {
+        return b.score - a.score;
+    });
+}
 
 module.exports = {
+    battle: function(players) {
+        //Returns a new array of all the requests
+        // profile and score from the user data request
+        return axios.all(players.map(getUserData))
+            .then(sortPlayers);
+    },
     fetchPopularRepos: function(language)
     {
         var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+
