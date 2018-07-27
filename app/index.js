0e5a7382b29928a56079f0a97e28f89b3e87b745
Dane Miller <unfaiyted@gmail.com>
2018-07-27 12:52:12 -0500

    moving app to own js and popular js files

--- app/index.js
+++ app/index.js
@@ -1,22 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-
+var PropTypes = require('prop-types');
 require('./index.css');
-
-
-// state
-// lifestyle event
-// UI
-
-class App extends React.Component {
-    render() {
-        return (
-            <div>
-                Hello World!
-            </div>
-        )
-    }
-}
+var App = require('./components/App');
 
 
 ReactDOM.render(
