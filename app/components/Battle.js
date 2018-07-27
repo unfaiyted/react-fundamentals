45e0faa1223f33a1f017f750f3e7bde6e08ff26d
Dane Miller <unfaiyted@gmail.com>
2018-07-27 15:25:27 -0500

    adding router

---
+++ app/components/Battle.js
@@ -0,0 +1,15 @@
+var React = require('react');
+
+
+
+class Battle extends React.Component {
+    render() {
+        return (
+                <div>
+                    BATTLEEE
+                </div>
+        )
+    }
+}
+
+module.exports = Battle;
